b3a8730df6f222fd2e0307146e9dadb696758437
nobden1989 <tang12h@uwindsor.ca>
2015-11-06 19:12:24 -0500

    update error code&message

--- Calendar/src/calendar/statics/ErrorCode.java
+++ Calendar/src/calendar/statics/ErrorCode.java
@@ -2,25 +2,29 @@ package calendar.statics;
 
 public class ErrorCode {
 	private final static String ERR_0000 = "Unkown Error.";
-	private final static String ERR_1001 = "Start time or end time is null object.";
-	private final static String ERR_1002 = "Start time must before end time.";
-	private final static String ERR_1003 = "Start time and end time must within the business hour.";
-	private final static String ERR_2001 = "Meeting time overlaped!";
+	private final static String ERR_1010 = "Start time or end time is null object.";
+	private final static String ERR_1020 = "Start time must before end time.";
+	private final static String ERR_1030 = "The meeting must shorter than an hour.";
+	private final static String ERR_1040 = "Start time and end time must within the business hour.";
+	private final static String ERR_2010 = "Meeting time overlaped!";
 
 	public static String getErrorMessage(int errorCode) {
 		String eMsg;
 		switch (errorCode) {
-		case 1001:
-			eMsg = ERR_1001;
+		case 1010:
+			eMsg = ERR_1010;
 			break;
-		case 1002:
-			eMsg = ERR_1002;
+		case 1020:
+			eMsg = ERR_1020;
 			break;
-		case 1003:
-			eMsg = ERR_1003;
+		case 1030:
+			eMsg = ERR_1030;
 			break;
-		case 2001:
-			eMsg = ERR_2001;
+		case 1040:
+			eMsg = ERR_1040;
+			break;
+		case 2010:
+			eMsg = ERR_2010;
 			break;
 		default:
 			eMsg = ERR_0000;
